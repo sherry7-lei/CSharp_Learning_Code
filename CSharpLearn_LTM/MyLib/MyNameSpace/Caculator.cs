e112c1f73fe2700b937a15290b699c88cd67e6c2
sherry7-lei <1085392111@qq.com>
2023-08-02 08:46:10 +0800

    Bilibili LTM Class

---
+++ CSharpLearn_LTM/MyLib/MyNameSpace/Caculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLib.MyNameSpaceT;
+
+namespace MyLib.MyNameSpace
+{
+    internal class Caculator
+    {
+        Student student;
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ CSharpLearn_LTM/MyLib/MyNameSpaceT/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib.MyNameSpaceT
+{
+    public class Teacher
+    {
+        public void Report()
+        {
+            Console.WriteLine("I'm a teacher!");
+        }
+    }
+
+    internal class Student
+    {
+        public void Report()
+        {
+            Console.WriteLine("I'm a teacher!");
+        }
+    }
+}
