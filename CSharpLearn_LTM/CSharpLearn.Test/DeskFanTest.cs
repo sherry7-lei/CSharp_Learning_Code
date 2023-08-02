a2d93efa7bc0ce6888b154dc08a7a87729fc46a4
sherry7-lei <1085392111@qq.com>
2023-08-02 08:45:29 +0800

    Bilibili LTM C# Interface_Test

---
+++ CSharpLearn_LTM/CSharpLearn.Test/DeskFanTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Moq;
+
+namespace CSharpLearn.Test
+{
+    // 第一种
+    /*
+    [TestClass]
+    public class DeskFanTest
+    {
+        [TestMethod]
+        public void PowerLowerThanZero_OK()
+        {
+            var fan = new DeskFan(new PowerSupplyLowerThanZero());
+            var expected = "Won't work.";
+            var actual = fan.Work();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerHigherThan200_Warning()
+        {
+            var fan = new DeskFan(new PowerSupplyHigherThan200());
+            // 注：此处为了演示，实际程序那边先故意改成了 Exploded!
+            var expected = "Warning";
+            var actual = fan.Work();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+    class PowerSupplyLowerThanZero : IPowerSupply
+    {
+        public int GetPower()
+        {
+            return 0;
+        }
+    }
+
+    class PowerSupplyHigherThan200 : IPowerSupply
+    {
+        public int GetPower()
+        {
+            return 220;
+        }
+    }
+    */
+
+    // 第二种
+    /*
+    [TestClass]
+    public class DeskFanTest
+    {
+        [TestMethod]
+        public void PowerLowerThanZero_OK()
+        {
+            var mock = new Mock<IPowerSupply>();
+            // 设置该 mock 对应的 GetPower 方法返回 0
+            mock.Setup(ps => ps.GetPower()).Returns(() => 0);
+            var fan = new DeskFan(mock.Object);
+            var expected = "Won't work.";
+            var actual = fan.Work();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void PowerHigherThan200_Warning()
+        {
+            var mock = new Mock<IPowerSupply>();
+            mock.Setup(ps => ps.GetPower()).Returns(() => 220);
+            var fan = new DeskFan(mock.Object);
+            var expected = "Warning";
+            var actual = fan.Work();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+    */
+}
---
+++ CSharpLearn_LTM/CSharpLearn.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSharpLearn.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSharpLearn.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8a2f0018-f8fc-4b77-b1c1-270c5c35ac63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
